aba300a5066d8c64f853fd8dee5f85c6d681b4c1
Christian Sauer <sauerc@gmail.com>
2016-08-19 09:34:11 -0400

    Replace Delete with CBV

--- avatar/urls.py
+++ avatar/urls.py
@@ -5,7 +5,7 @@ from avatar import views
 urlpatterns = [
     url(r'^add/$', views.add, name='avatar_add'),
     url(r'^change/$', views.change, name='avatar_change'),
-    url(r'^delete/$', views.delete, name='avatar_delete'),
+    url(r'^delete/$', views.Delete.as_view(), name='avatar_delete'),
     url(r'^render_primary/(?P<user>[\w\d\@\.\-_]+)/(?P<size>[\d]+)/$',
         views.render_primary,
         name='avatar_render_primary'),
