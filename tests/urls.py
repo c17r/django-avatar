0182a9c4a7ff9511dfe5cd7d7d4a49518180a4ed
Christian Sauer <sauerc@gmail.com>
2016-08-19 09:54:30 -0400

    Replace add with CBV
    Actual add functionality in Base since we'll need it in change view as
    well

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import include, url
+from django.core.urlresolvers import reverse_lazy
+from avatar import views
+from django.views import generic
 
 
 urlpatterns = [
     url(r'^avatar/', include('avatar.urls')),
+
+    url(r'^landing_page/$', generic.TemplateView.as_view(template_name='dummy/success.html'), name='landing_page'),
+
+    url(r'^success_url/add/$', views.Add.as_view(success_url=reverse_lazy('landing_page')), name='success_url_add'),
 ]
