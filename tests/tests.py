856fe78777b00bbfd009f16e1dd7daf6899fc860
Christian Sauer <sauerc@gmail.com>
2016-08-19 10:23:33 -0400

    Tests for alternate template
    These really aren't needed since CBVs auto-magically gives us the
    ability to override the template.  Upshot of added these in is they take
    us to 100% coverage in the views.

--- tests/tests.py
+++ tests/tests.py
@@ -258,7 +258,35 @@ class AvatarTests(TestCase):
         self.assertIn('<img src="{}"'.format(avatar.avatar_url(100)), result)
         self.assertIn('alt="test" width="100" height="100" />', result)
 
-    # def testAvatarOrder
+    def test_default_add_template(self):
+        response = self.client.get(reverse('avatar_add'))
+        self.assertContains(response, 'Upload New Image')
+        self.assertNotContains(response, 'ALTERNATE ADD TEMPLATE')
+
+    def test_custom_add_template(self):
+        response = self.client.get(reverse('custom_add'))
+        self.assertNotContains(response, 'Upload New Image')
+        self.assertContains(response, 'ALTERNATE ADD TEMPLATE')
+
+    def test_default_change_template(self):
+        response = self.client.get(reverse('avatar_change'))
+        self.assertContains(response, 'Upload New Image')
+        self.assertNotContains(response, 'ALTERNATE CHANGE TEMPLATE')
+
+    def test_custom_change_template(self):
+        response = self.client.get(reverse('custom_change'))
+        self.assertNotContains(response, 'Upload New Image')
+        self.assertContains(response, 'ALTERNATE CHANGE TEMPLATE')
+
+    def test_default_delete_template(self):
+        response = self.client.get(reverse('avatar_delete'))
+        self.assertContains(response, 'like to delete.')
+        self.assertNotContains(response, 'ALTERNATE DELETE TEMPLATE')
+
+    def test_custom_delete_template(self):
+        response = self.client.get(reverse('custom_delete'))
+        self.assertNotContains(response, 'like to delete.')
+        self.assertContains(response, 'ALTERNATE DELETE TEMPLATE')    # def testAvatarOrder
     # def testReplaceAvatarWhenMaxIsOne
     # def testHashFileName
     # def testHashUserName
